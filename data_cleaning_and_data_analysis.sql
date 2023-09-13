da32b1aa7a5a27fb961d386768819f46ef92cf0a
Amisha2912 <143608573+Amisha2912@users.noreply.github.com>
2023-09-13 09:29:32 -0700

    data_cleaning_and_data_analysis.sql

--- data_cleaning_and_data_analysis.sql
+++ data_cleaning_and_data_analysis.sql
@@ -1,14 +1,21 @@
+CREATE DATABASE firstproject;
 USE firstproject;
 
 SELECT * FROM hr;
 
+/* DATA CLEANING */
+
+/* changing column name from  ï»¿id to emp_id */
 ALTER TABLE hr
 CHANGE COLUMN ï»¿id emp_id VARCHAR(20) NULL;
 DESCRIBE hr;
 
 SELECT birthdate FROM hr;
 
+/* to disable safe mode in MySQL */
 SET sql_safe_updates =0;
+
+/* changing the format of the date into YYYY-MM-DD*/
 UPDATE hr
 SET birthdate = CASE
   WHEN birthdate LIKE '%/%' THEN date_format(str_to_date(birthdate,'%m/%d/%Y'),'%Y-%m-%d')
@@ -16,9 +23,12 @@ SET birthdate = CASE
   ELSE NULL
 END;
 
+/* modifying the data type to DATE */
 ALTER TABLE hr
 MODIFY COLUMN birthdate DATE;
 
+
+/* changing the format of the date into YYYY-MM-DD*/
 UPDATE hr
 SET hire_date = CASE
   WHEN hire_date LIKE '%/%' THEN date_format(str_to_date(hire_date,'%m/%d/%Y'),'%Y-%m-%d')
@@ -26,11 +36,14 @@ SET hire_date = CASE
   ELSE NULL
 END;
 
+/* modifying the data type to DATE */
 ALTER TABLE hr
 MODIFY COLUMN hire_date DATE;
 
 SELECT hire_date FROM hr;
 
+
+/* changing the format of the date into YYYY-MM-DD under conditions*/ 
 UPDATE hr
 SET termdate = date(str_to_date(termdate,'%Y-%m-%d %H:%i:%s UTC'))
 WHERE termdate IS NOT NULL AND termdate !='';
@@ -41,11 +54,16 @@ WHERE termdate IS NULL OR termdate = '';
 
 SELECT termdate FROM hr;
 
+
+/* modifying the data type to DATE */
 ALTER TABLE hr
 MODIFY COLUMN termdate DATE;
 
+
+/* adding new column 'age'*/
 ALTER TABLE hr ADD COLUMN age INT;
 
+/* adding values in the column 'age'*/
 UPDATE hr
 SET age= timestampdiff(YEAR,birthdate,CURDATE());
 
@@ -56,16 +74,12 @@ FROM hr;
   
 SELECT birthdate,age FROM hr;
 
+/* DATA ANALYSIS*/
+/* gender breakdown and no. of employees in it*/
 SELECT gender, COUNT(*) AS count
 FROM hr
 WHERE age >= 18 AND termdate IS NULL
 GROUP BY gender;
-
-SELECT race, COUNT(*) AS count
-FROM hr
-WHERE age >= 18 AND termdate IS NULL
-GROUP BY race
-ORDER BY count DESC;
 
 SELECT 
   min(age) AS youngest,
@@ -73,6 +87,8 @@ SELECT
 FROM hr
 WHERE age >= 18 AND termdate IS NULL;
 
+
+/* age group distribution with no. of employees under conditions */
 SELECT
  CASE
    WHEN age >= 18 AND age <=24 THEN '18-24'
@@ -105,18 +121,19 @@ GROUP BY age_group,gender
 ORDER BY age_group,gender;
 
 
+/*EMPLOYEES BASED ON THEIR LOCATIONS*/
 SELECT location,count(*) AS count
 FROM hr
 WHERE age>= 18 AND termdate IS NULL
 GROUP BY location;
 
-
+/*Average length of the employment for employees who have been terminated */
 SELECT 
  round(avg(datediff(termdate,hire_date))/365,0) AS avg_length_employment
 FROM hr
 WHERE termdate IS NOT NULL AND termdate <= CURDATE() AND age >= 18; 
 
-
+/* Employees distribution  based on the department and job title*/
 SELECT department,gender,COUNT(*) AS count
 FROM hr
 WHERE age >= 18 AND termdate IS NULL
@@ -131,6 +148,7 @@ GROUP BY jobtitle
 ORDER BY jobtitle DESC;
 
 
+/* displaying which department has highest turnover rate*/
 SELECT department,
  total_count,
  terminate_count,
@@ -146,13 +164,14 @@ SELECT department,
 ORDER BY termination_rate DESC;   
 
 
+/* location of employees based o  their states*/
 SELECT location_state,COUNT(*) AS count
 FROM hr
 WHERE age>= 18 AND termdate IS NULL
 GROUP BY location_state
 ORDER BY count DESC;
 
-
+/*net change in employees count based on the hiring and termination of employees */
 SELECT 
   year,
   hires,
@@ -170,7 +189,7 @@ FROM(
 ORDER BY year ASC;  
 
 
-
+/* Tenure distribution for each department */
 SELECT department,round(avg(datediff(termdate,hire_date)/365),0) AS avg_tenure
 FROM hr
 WHERE termdate IS NOT NULL AND termdate <= CURDATE() AND age >= 18
